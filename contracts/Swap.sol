c895ee717e09958c1ffe064294a55a8cc95a0abe
hdsfade <552037917@qq.com>
2023-03-31 21:35:21 +0800

    Add Swap for handling uniswap

---
+++ contracts/Swap.sol
@@ -0,0 +1,153 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import {ISwapRouter} from "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
+import {IERC20} from "./interfaces/IERC20.sol";
+
+contract Swap {
+    struct SwapParams {
+        bytes path;
+        bool single;
+        address recipient;
+        uint256 amountIn;
+        uint256 amountOutMinimum;
+    }
+
+    /// @dev The length of the bytes encoded address
+    uint256 private constant ADDR_SIZE = 20;
+    /// @dev The length of the bytes encoded fee
+    uint256 private constant FEE_SIZE = 3;
+    /// @dev The offset of a single token address and pool fee
+    uint256 private constant NEXT_OFFSET = ADDR_SIZE + FEE_SIZE;
+
+    ISwapRouter public swapRouter;
+
+    constructor(ISwapRouter _swapRouter) {
+        swapRouter = _swapRouter;
+    }
+
+    function swap(
+        SwapParams memory swapParams
+    ) public returns (uint256 amountOut) {
+        if (swapParams.single) {
+            amountOut = swapExactInputSingle(
+                swapParams.path,
+                swapParams.recipient,
+                swapParams.amountIn,
+                swapParams.amountOutMinimum
+            );
+        } else {
+            amountOut = swapExactInput(
+                swapParams.path,
+                swapParams.recipient,
+                swapParams.amountIn,
+                swapParams.amountOutMinimum
+            );
+        }
+    }
+
+    function swapExactInputSingle(
+        bytes memory path,
+        address recipient,
+        uint256 amountIn,
+        uint256 amountOutMinimum
+    ) internal returns (uint256 amountOut) {
+        (address tokenIn, address tokenOut, uint24 fee) = decodeFirstPool(path);
+
+        _safeApprove(tokenIn, address(swapRouter), amountIn);
+        IERC20(tokenIn).transferFrom(msg.sender, address(this), amountIn);
+
+        ISwapRouter.ExactInputSingleParams memory params = ISwapRouter
+            .ExactInputSingleParams({
+                tokenIn: tokenIn,
+                tokenOut: tokenOut,
+                fee: fee,
+                recipient: recipient,
+                deadline: block.timestamp + 3000,
+                amountIn: amountIn,
+                amountOutMinimum: amountOutMinimum,
+                sqrtPriceLimitX96: 0
+            });
+
+        amountOut = swapRouter.exactInputSingle(params);
+    }
+
+    function swapExactInput(
+        bytes memory path,
+        address recipient,
+        uint256 amountIn,
+        uint256 amountOutMinimum
+    ) internal returns (uint256 amountOut) {
+        (address tokenIn, , ) = decodeFirstPool(path);
+
+        _safeApprove(tokenIn, address(swapRouter), amountIn);
+        IERC20(tokenIn).transferFrom(msg.sender, address(this), amountIn);
+
+        ISwapRouter.ExactInputParams memory params = ISwapRouter
+            .ExactInputParams({
+                path: path,
+                recipient: recipient,
+                deadline: block.timestamp + 3000,
+                amountIn: amountIn,
+                amountOutMinimum: amountOutMinimum
+            });
+
+        amountOut = swapRouter.exactInput(params);
+    }
+
+    /// @notice Decodes the first pool in path
+    /// @param path The bytes encoded swap path
+    /// @return tokenA The first token of the given pool
+    /// @return tokenB The second token of the given pool
+    /// @return fee The fee level of the pool
+    function decodeFirstPool(
+        bytes memory path
+    ) internal pure returns (address tokenA, address tokenB, uint24 fee) {
+        tokenA = toAddress(path, 0);
+        fee = toUint24(path, ADDR_SIZE);
+        tokenB = toAddress(path, NEXT_OFFSET);
+    }
+
+    function toAddress(
+        bytes memory _bytes,
+        uint256 _start
+    ) internal pure returns (address) {
+        require(_start + 20 >= _start, "toAddress_overflow");
+        require(_bytes.length >= _start + 20, "toAddress_outOfBounds");
+        address tempAddress;
+
+        assembly {
+            tempAddress := div(
+                mload(add(add(_bytes, 0x20), _start)),
+                0x1000000000000000000000000
+            )
+        }
+
+        return tempAddress;
+    }
+
+    function toUint24(
+        bytes memory _bytes,
+        uint256 _start
+    ) internal pure returns (uint24) {
+        require(_start + 3 >= _start, "toUint24_overflow");
+        require(_bytes.length >= _start + 3, "toUint24_outOfBounds");
+        uint24 tempUint;
+
+        assembly {
+            tempUint := mload(add(add(_bytes, 0x3), _start))
+        }
+
+        return tempUint;
+    }
+
+    function _safeApprove(address token, address to, uint256 value) internal {
+        (bool success, bytes memory data) = token.call(
+            abi.encodeWithSelector(IERC20.approve.selector, to, value)
+        );
+        require(
+            success && (data.length == 0 || abi.decode(data, (bool))),
+            "SA"
+        );
+    }
+}
